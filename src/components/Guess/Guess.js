83a06139fc0fd1b3bfc17b6b1a6da811729a07b4
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2023-03-15 14:33:44 +0100

    Style the letter according to their status

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { checkGuess } from '../../game-helpers';
 import { range } from '../../utils';
 
-function Guess({ guess }) {
+function Guess({ guess, answer }) {
   const guessLetters = range(5);
+  const validatedGuess = checkGuess(guess, answer);
 
-  const cells = guessLetters.map((letterIndex) => (
-    <span className='cell' key={letterIndex}>
-      {guess?.[letterIndex] || ''}
-    </span>
-  ));
+  const cells = guessLetters.map((letterIndex) => {
+    const className = `cell ${validatedGuess?.[letterIndex]?.status || ''}`;
+
+    return (
+      <span className={className} key={letterIndex}>
+        {guess?.[letterIndex] || ''}
+      </span>
+    );
+  });
+
   return <p className='guess'>{cells}</p>;
 }
 
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,11 +3,11 @@ import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { range } from '../../utils';
 import Guess from '../Guess/Guess';
 
-function GuessResults({ guessResults }) {
+function GuessResults({ guessResults, answer }) {
   const allowedGuesses = range(NUM_OF_GUESSES_ALLOWED);
 
   const resultElements = allowedGuesses.map((guessIndex) => (
-    <Guess key={guessIndex} guess={guessResults[guessIndex]} />
+    <Guess key={guessIndex} guess={guessResults[guessIndex]} answer={answer} />
   ));
 
   return <div className='guess-results'>{resultElements}</div>;
