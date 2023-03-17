ad7a12362aa82a302890f1159416dc58b59afb68
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2023-03-17 13:31:51 +0100

    Added ability to Restart

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
-function HappyBanner({ noOfGuesses }) {
+function HappyBanner({ noOfGuesses, restartGame }) {
   return (
     <div className='banner happy'>
       <p>
         <strong>Congratulations!</strong> Got it in
         <strong> {noOfGuesses} guesses</strong>.
       </p>
+
+      <button className='btn btn-restart' onClick={restartGame}>
+        {' '}
+        Restart
+      </button>
     </div>
   );
 }
 
-function SadBanner({ answer }) {
+function SadBanner({ answer, restartGame }) {
   return (
     <div className='banner sad'>
       <p>
         <strong>Sorry!</strong> You ran out of guesses. The correct answer was{' '}
         <strong>{answer}</strong>.
       </p>
+
+      <button className='btn btn-restart' onClick={restartGame}>
+        {' '}
+        Restart
+      </button>
     </div>
   );
 }
 
-function Banner({ lastGuess, answer, noOfGuesses }) {
+function Banner({ lastGuess, answer, noOfGuesses, restartGame }) {
   const showBanner = lastGuess === answer || noOfGuesses === NUM_OF_GUESSES_ALLOWED;
   if (!showBanner) {
     return null;
@@ -31,9 +41,9 @@ function Banner({ lastGuess, answer, noOfGuesses }) {
 
   const isGameWon = lastGuess === answer;
   return isGameWon === true ? (
-    <HappyBanner noOfGuesses={noOfGuesses} />
+    <HappyBanner noOfGuesses={noOfGuesses} restartGame={restartGame} />
   ) : (
-    <SadBanner answer={answer} />
+    <SadBanner answer={answer} restartGame={restartGame} />
   );
 }
 
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -9,10 +9,8 @@ import GuessInput from '../GuessInput';
 import GuessResults from '../GuessResults';
 import Keyboard from '../Keyboard';
 
-// Pick a random word on every page load.
-const answer = sample(WORDS);
-
 function Game() {
+  const [answer, setAnswer] = React.useState(sample(WORDS));
   const [guessResults, setGuessResults] = React.useState([]);
   const [guessedLetters, setGuessedLetters] = React.useState([]);
 
@@ -23,13 +21,24 @@ function Game() {
     setGuessResults((guessResults) => [...guessResults, guess]);
   };
 
+  const restartGame = () => {
+    setAnswer(sample(WORDS));
+    setGuessResults([]);
+    setGuessedLetters([]);
+  };
+
   return (
     <>
       <GuessResults guessResults={guessResults} answer={answer} />
       <GuessInput addGuess={addGuess} disabled={guessResults.length === NUM_OF_GUESSES_ALLOWED} />
       <Keyboard guessedLetters={guessedLetters} />
 
-      <Banner noOfGuesses={guessResults.length} answer={answer} lastGuess={guessResults.at(-1)} />
+      <Banner
+        noOfGuesses={guessResults.length}
+        answer={answer}
+        lastGuess={guessResults.at(-1)}
+        restartGame={restartGame}
+      />
     </>
   );
 }
